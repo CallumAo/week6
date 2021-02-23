d25208af275ff5eec9f69c11aba41ffa3d15e40a
andrew <andrewattwood@gmail.com>
2021-02-23 12:53:47 +0000

    Update mongo.js

---
+++ mongo.js
@@ -0,0 +1,75 @@
+
+//Object data modelling library for mongo
+const mongoose = require('mongoose');
+
+//Mongo db client library
+//const MongoClient  = require('mongodb');
+
+//Express web service library
+const express = require('express')
+
+//used to parse the server response from json to object.
+const bodyParser = require('body-parser');
+
+//instance of express and port to use for inbound connections.
+const app = express()
+const port = 3000
+
+//connection string listing the mongo servers. This is an alternative to using a load balancer. THIS SHOULD BE DISCUSSED IN YOUR ASSIGNMENT.
+const connectionString = 'mongodb://192.168.1.106:40001,192.168.1.106:40002,192.168.1.106:40003/myCompany?replicaSet=cfgrs';
+
+setInterval(function() {
+
+  console.log(`Intervals are used to fire a function for the lifetime of an application.`);
+
+}, 3000);
+
+//tell express to use the body parser. Note - This function was built into express but then moved to a seperate package.
+app.use(bodyParser.json());
+
+//connect to the cluster
+mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var Schema = mongoose.Schema;
+
+var SomeModelSchema = new Schema({
+  firstName: String,
+  lastName: String
+});
+
+var SomeModel = mongoose.model('SomeModel', SomeModelSchema );
+
+
+
+app.get('/', (req, res) => {
+  SomeModel.find({},'firstName lastName', (err, staff) => {
+    if(err) return handleError(err);
+    res.send(JSON.stringify(staff))
+  }) 
+})
+
+app.post('/',  (req, res) => {
+  var awesome_instance = new SomeModel(req.body);
+  awesome_instance.save(function (err) {
+  if (err) res.send('Error');
+    res.send(JSON.stringify(req.body))
+  });
+})
+
+app.put('/',  (req, res) => {
+  res.send('Got a PUT request at /')
+})
+
+app.delete('/',  (req, res) => {
+  res.send('Got a DELETE request at /')
+})
+
+//bind the express web service to the port specified
+app.listen(port, () => {
+ console.log(`Express Application listening at port ` + port)
+})
+
